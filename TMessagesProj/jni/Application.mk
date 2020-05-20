3354beeea4613d77249f2bf5c39aeff057d47604
juliandresbv <ja.bermudez10@uniandes.edu.co>
2020-05-20 13:49:43 -0500

    WindOS Comp: build.gradle and Application.mk files modified for compiling in Windows OS

--- TMessagesProj/jni/Application.mk
+++ TMessagesProj/jni/Application.mk
@@ -1,3 +1,7 @@
 APP_PLATFORM := android-16
 NDK_TOOLCHAIN_VERSION := clang
 APP_STL := c++_static
+
+# WinOS Comp:
+# APP_SHORT_COMMANDS handles long command errors when compiling in Windows OS
+APP_SHORT_COMMANDS := true
